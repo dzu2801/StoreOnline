5ab8d3243483c15bdf9b39f88b469a8e6e334106
dzu2801 <34890975+dzu2801@users.noreply.github.com>
2018-04-23 12:49:38 +0700

    lan 3

--- StoreOnline/Controllers/ShoponlineController.cs
+++ StoreOnline/Controllers/ShoponlineController.cs
@@ -18,5 +18,10 @@ namespace StoreOnline.Controllers
         {
             return View();
         }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
     }
 }
