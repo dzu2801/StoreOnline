d611b10b86fddefb6aba690b9682c2895b7871d2
dzu2801 <34890975+dzu2801@users.noreply.github.com>
2018-04-23 12:25:27 +0700

    lan 2

--- StoreOnline/App_Start/RouteConfig.cs
+++ StoreOnline/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace StoreOnline
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Shoponline", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
--- StoreOnline/Controllers/ShoponlineController.cs
+++ StoreOnline/Controllers/ShoponlineController.cs
@@ -13,5 +13,10 @@ namespace StoreOnline.Controllers
         {
             return View();
         }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
     }
 }
