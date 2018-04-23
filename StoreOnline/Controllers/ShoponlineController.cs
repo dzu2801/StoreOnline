184d2ab8bba027f5ff479356a6482f09d80872eb
dzu2801 <34890975+dzu2801@users.noreply.github.com>
2018-04-23 10:48:07 +0700

    Do An

---
+++ StoreOnline/Controllers/ShoponlineController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StoreOnline.Controllers
+{
+    public class ShoponlineController : Controller
+    {
+        // GET: Shoponline
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
