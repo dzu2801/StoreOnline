674a03f9892cc7518fb5454fd1bf6944736dde25
dzu2801 <34890975+dzu2801@users.noreply.github.com>
2018-04-27 00:11:49 +0700

    update

--- StoreOnline/Controllers/ShoponlineController.cs
+++ StoreOnline/Controllers/ShoponlineController.cs
@@ -4,17 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using StoreOnline.Models;
+using PagedList;
+using PagedList.Mvc;
 
 namespace StoreOnline.Controllers
 {
     public class ShoponlineController : Controller
     {
         dbStoreOnlineDataContext db = new dbStoreOnlineDataContext();
-
-        //private List<SANPHAM> SanPhamHot(int count)
-        //{
-        //    return db.SANPHAMs.OrderByDescending(a => a.DONDATHANGs).Take(count).ToList();
-        //}
 
         // GET: Shoponline
         //Tao doi tuong daya chưa dữ liệu từ model dbBansach đã tạo. 
@@ -23,11 +20,13 @@ namespace StoreOnline.Controllers
             //Sắp xếp sách theo ngày cập nhật, sau đó lấy top @count 
             return db.SANPHAMs.OrderByDescending(a => a.TENSP).Take(count).ToList();
         }
-        public ActionResult Index()
+        public ActionResult Index(int ? page)
         {
+            int pageSize = 12;
+            int pageNum = (page ?? 1);
             //Lấy top 5 Album bán chạy nhất
             var sanpham = Laysanpham(30);
-            return View(sanpham);
+            return View(sanpham.ToPagedList(pageNum, pageSize));
         }        
         public ActionResult Contact()
         {
