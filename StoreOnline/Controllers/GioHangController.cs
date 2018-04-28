32c9c63d72e1d3a4c9d54faf5f41d9d75c44e72d
ledinhmanh07 <38520257+ledinhmanh07@users.noreply.github.com>
2018-04-28 10:05:55 +0700

    mạnh
    sửa

---
+++ StoreOnline/Controllers/GioHangController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StoreOnline.Models;
+namespace StoreOnline.Controllers
+{
+    public class GioHangController : Controller
+    {
+        //Tao doi tuong data chua dữ liệu từ model dbStoreOnline đã tạo. 
+        dbStoreOnlineDataContext data = new dbStoreOnlineDataContext();
+        //Lay gio hang
+        public List<Giohang> Laygiohang()
+        {
+            List<Giohang> lstGiohang = Session["Giohang"] as List<Giohang>;
+            if (lstGiohang == null)
+            {
+                //Neu gio hang chua ton tai thi khoi tao listGiohang
+                lstGiohang = new List<Giohang>();
+                Session["Giohang"] = lstGiohang;
+            }
+            return lstGiohang;
+        }
+        //Them hang vao gio
+        public ActionResult ThemGiohang(string iMasp, string strURL)
+        {
+            //Lay ra Session gio hang
+            List<Giohang> lstGiohang = Laygiohang();
+            //Kiem tra sách này tồn tại trong Session["Giohang"] chưa?
+            Giohang sanpham = lstGiohang.Find(n => n.iMasp == iMasp);
+            if (sanpham == null)
+            {
+                sanpham = new Giohang(iMasp);
+                lstGiohang.Add(sanpham);
+                return Redirect(strURL);
+            }
+            else
+            {
+                sanpham.iSoluong++;
+                return Redirect(strURL);
+            }
+        }
+
+
+        //Xay dung trang Gio hang
+        public ActionResult GioHang()
+        {
+            List<Giohang> lstGiohang = Laygiohang();
+            if (lstGiohang.Count == 0)
+            {
+                return RedirectToAction("Index", "Shoponline");
+            }
+            ViewBag.Tongsoluong = TongSoLuong();
+            ViewBag.Tongtien = TongTien();
+            return View(lstGiohang);
+        }
+        //Tong so luong
+        private int TongSoLuong()
+        {
+            int iTongSoLuong = 0;
+            List<Giohang> lstGiohang = Session["GioHang"] as List<Giohang>;
+            if (lstGiohang != null)
+            {
+                iTongSoLuong = lstGiohang.Sum(n => n.iSoluong);
+            }
+            return iTongSoLuong;
+        }
+        //Tinh tong tien
+        private double TongTien()
+        {
+            double iTongTien = 0;
+            List<Giohang> lstGiohang = Session["GioHang"] as List<Giohang>;
+            if (lstGiohang != null)
+            {
+                iTongTien = lstGiohang.Sum(n => n.dThanhtien);
+            }
+            return iTongTien;
+        }
+        //Tao Partial view de hien thi thong tin gio hang
+        public ActionResult GiohangPartial()
+        {
+            ViewBag.Tongsoluong = TongSoLuong();
+            ViewBag.Tongtien = TongTien();
+            return PartialView();
+        }
+        //Cap nhat Giỏ hàng
+        public ActionResult CapnhatGiohang(string iMaSP, FormCollection f)
+        {
+
+            //Lay gio hang tu Session
+            List<Giohang> lstGiohang = Laygiohang();
+            //Kiem tra sach da co trong Session["Giohang"]
+            Giohang sanpham = lstGiohang.SingleOrDefault(n => n.iMasp == iMaSP);
+            //Neu ton tai thi cho sua Soluong
+            if (sanpham != null)
+            {
+                sanpham.iSoluong = int.Parse(f["txtSoluong"].ToString());
+            }
+            return RedirectToAction("Giohang");
+        }
+        //Xoa Giohang
+        public ActionResult XoaGiohang(string iMaSP)
+        {
+            //Lay gio hang tu Session
+            List<Giohang> lstGiohang = Laygiohang();
+            //Kiem tra sach da co trong Session["Giohang"]
+            Giohang sanpham = lstGiohang.SingleOrDefault(n => n.iMasp == iMaSP);
+            //Neu ton tai thi cho sua Soluong
+            if (sanpham != null)
+            {
+                lstGiohang.RemoveAll(n => n.iMasp == iMaSP);
+                return RedirectToAction("GioHang");
+
+            }
+            if (lstGiohang.Count == 0)
+            {
+                return RedirectToAction("Index", "Shoponline");
+            }
+            return RedirectToAction("GioHang");
+        }
+        //Xoa tat ca thong tin trong Gio hang
+        public ActionResult XoaTatcaGiohang()
+        {
+            //Lay gio hang tu Session
+            List<Giohang> lstGiohang = Laygiohang();
+            lstGiohang.Clear();
+            return RedirectToAction("Index", "Shoponline");
+        }
+    }
+}
---
+++ StoreOnline/Models/Giohang.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StoreOnline.Models
+{
+    public class Giohang
+    {
+        //Tao doi tuong data chua dữ liệu từ model dbStoreOnline đã tạo. 
+         dbStoreOnlineDataContext data = new dbStoreOnlineDataContext();
+        public string iMasp { set; get; }
+        public string sTensp { set; get; }
+        public string sAnhsp { set; get; }
+        public Double dDongia { set; get; }
+        public int iSoluong { set; get; }
+        public Double dThanhtien
+        {
+            get { return iSoluong * dDongia; }
+
+        }
+        //Khoi tao gio hàng theo Masp duoc truyen vao voi Soluong mac dinh la 1
+        public Giohang(string Masach)
+        {
+            iMasp = Masach;
+            SANPHAM sp = data.SANPHAMs.Single(n => n.MASP == iMasp);
+            sTensp = sp.TENSP;
+            sAnhsp = sp.HINHANH;
+            dDongia = double.Parse(sp.GIABAN.ToString());
+            iSoluong = 1;
+        }
+    }
+}
