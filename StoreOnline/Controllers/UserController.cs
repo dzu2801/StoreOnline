97c75a6c5164e08ebf9ec7ceb92a9b38dbd28adf
dzu2801 <34890975+dzu2801@users.noreply.github.com>
2018-04-28 22:33:21 +0700

    update-07-28/04

--- StoreOnline/Controllers/UserController.cs
+++ StoreOnline/Controllers/UserController.cs
@@ -26,38 +26,58 @@ namespace StoreOnline.Controllers
         public ActionResult Register(FormCollection collection, KHACHHANG kh)
         {
             var taikhoan = collection["Username"];
+            var tenkh = collection["Name"];
             var email = collection["Email"];
+            var diachi = collection["Add"];
+            var dienthoai = collection["Phone"];
+            var gioitinh = collection["Sex"];
             var matkhau = collection["Password"];
             var matkhaunl = collection["Confirm Password"];
             if (Convert.ToString(taikhoan) == "Username")
             {
                 ViewData["loi1"] = "Không được bỏ trống";
             }
-            else if (Convert.ToString(email) == "Email")
+            else if (Convert.ToString(tenkh) == "Name")
             {
                 ViewData["loi2"] = "Không được bỏ trống";
             }
-            else if (Convert.ToString(matkhau) == "Password")
+            else if (Convert.ToString(email) == "Email")
             {
                 ViewData["loi3"] = "Không được bỏ trống";
             }
-            else if (Convert.ToString(matkhaunl) == "Confirm Password")
+            else if (Convert.ToString(diachi) == "Add")
             {
                 ViewData["loi4"] = "Không được bỏ trống";
             }
+            else if (Convert.ToString(dienthoai) == "Phone")
+            {
+                ViewData["loi5"] = "Không được bỏ trống";
+            }
+            else if (Convert.ToString(gioitinh) == "Sex")
+            {
+                ViewData["loi6"] = "Không được bỏ trống";
+            }
+            else if (Convert.ToString(matkhau) == "Password")
+            {
+                ViewData["loi7"] = "Không được bỏ trống";
+            }
+            else if (Convert.ToString(matkhaunl) == "Confirm Password")
+            {
+                ViewData["loi8"] = "Không được bỏ trống";
+            }
             else if (Convert.ToString(matkhau) != Convert.ToString(matkhaunl))
             {
-                ViewData["loi4"] = "Mật khẩu nhập lại phải trùng trước đó";
+                ViewData["loi9"] = "Mật khẩu nhập lại phải trùng trước đó";
             }
             else
             {
                 kh.TK = taikhoan;
                 kh.MK = matkhau;
                 kh.EMAILKH = email;
-                kh.TENKH = null;
-                kh.GIOITINH = null;
-                kh.DIACHIKH = null;
-                kh.DTKH = null;
+                kh.TENKH = tenkh;
+                kh.GIOITINH = gioitinh;
+                kh.DIACHIKH = diachi;
+                kh.DTKH = dienthoai;
                 kh.MAKH = "";
                 db.KHACHHANGs.InsertOnSubmit(kh);
                 db.SubmitChanges();
